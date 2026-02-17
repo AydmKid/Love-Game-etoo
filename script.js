6967ae30966c8013ecf48ca49695d6d768a22e90
Tekno <56026345+AydmKid@users.noreply.github.com>
2026-02-17 13:53:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function createNameBackground() {
 
   nameBackground.innerHTML = "";
 
-  const names = ["Etoo", "Lolo"];
+  const names = ["Tekno", "Bayan"];
   const totalNames = 26;
 
   for (let i = 0; i < totalNames; i += 1) {
